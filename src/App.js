1b1c78f319147af96fe176aa1ccf17c5636dd6cd
Syeda Nahal Mansoor <nahalsyeda@gmail.com>
2025-10-26 16:00:26 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [note , setNote] = useState([
+    { value : "Your are very Cute!" , disabled : true} ,
+    { value : "May Allah Bless You!" , disabled : true}
+  ])
+  let [value , setValue] = useState("")
+
+  let addNote = () => {
+    if (value.trim() === "") return;
+    setNote (
+      [...note , {value , disabled : true}]
+    )
+    setValue("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Note Book</h1>
+      <input type='text' placeholder='Type your note here......' onChange={(e) => setValue(e.target.value)} value={value}/>
+      <button onClick={addNote}>Add Note</button>
+      <button onClick={() => setNote([])}>Delete all notes </button> <br/>
+      <ul>
+        {
+          note.map ((v,i) => <li key = {i}>
+          <input defaultValue={v.value} disabled = {v.disabled}/>  
+          <button onClick={() => {
+            let allNote = [...note]
+            allNote.splice(i , 1)
+            setNote(allNote)
+          }}>Delete</button>
+          {
+            v.disabled === true ? 
+            <button onClick={() => {
+            let allNote = [...note]
+            allNote.splice(i , 1 , {...v , disabled : false})
+            setNote(allNote)
+            }}>Edit</button> : 
+
+            <button onClick={() => {
+              let allNote = [...note]
+              allNote.splice(i , 1 , {...v , disabled : true})
+              setNote(allNote)
+            }}>Update</button>
+          }
+          </li>)
+        }
+      </ul>
     </div>
   );
 }
 
 export default App;
+
